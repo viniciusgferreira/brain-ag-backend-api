5616e71ff284a50c4bebf3b71e73c4a732328404
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-20 06:30:57 -0300

    new file structure and app folder

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ import express from 'express';
 const port = 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Get its working');
-});
-
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
