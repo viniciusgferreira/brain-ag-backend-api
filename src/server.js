fa504db065a8efd85a62b2614b05d1240793425f
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-22 19:54:38 -0300

    add body parser json

--- src/server.js
+++ src/server.js
@@ -5,8 +5,13 @@ import { router } from './app/routes/router.js';
 const port = 3000;
 const app = express();
 
+//BODY PARSER JSON
+app.use(express.json());
+
 //CONTROLLER
 app.use(router);
+
+
 
 //SERVER START
 app.listen(port, () => {
