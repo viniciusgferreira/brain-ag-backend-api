0cc3c95219a86aa1c601605b98afea4533e8025e
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-22 07:21:31 -0300

    fix: req params into query without variable

--- src/app/services/farms/listFarmById.js
+++ src/app/services/farms/listFarmById.js
@@ -2,8 +2,7 @@ import { pool } from '../../models/farms-db.js';
 
 //Send json to user from database
 export default async function listFarms(req, res) {
-	const id = parseInt(req.params.id);
-	pool.query('SELECT * FROM farms WHERE id = $1', [id], (err, results) => {
+	pool.query('SELECT * FROM farms WHERE id = $1', [req.params.id], (err, results) => {
 		if (err) {
 			console.log(err);
 			res.status(400).send(err).end();
