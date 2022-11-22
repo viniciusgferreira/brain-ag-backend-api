71ade00cfa09f9e93917238cb9c7faaab8cb3082
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-22 19:57:44 -0300

    addFarm route for post insertion of new farms

--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import addFarm from '../services/farms/addFarm.js';
 import listFarmById from '../services/farms/listFarmById.js';
 import listFarms from '../services/farms/listFarms.js';
 
@@ -7,8 +8,9 @@ export const router = Router();
 //LIST FARMS
 router.get('/farms', listFarms);
 router.get('/farms/:id', listFarmById);
-//ADD NEW FARM
 
+//ADD NEW FARM
+router.post('/farms', addFarm);
 //EDIT FARM INFO
 
 //DELETE FARM
