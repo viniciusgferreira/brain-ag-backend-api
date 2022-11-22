bad0012b1110ba31f5b626e6915f3c5cd8211c03
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-22 06:45:31 -0300

    fix: handle bad request

--- src/app/services/farms/listFarms.js
+++ src/app/services/farms/listFarms.js
@@ -3,9 +3,12 @@ import { pool } from '../../models/farms-db.js';
 //Send json to user from database
 export default async function listFarms(req, res) {
 	pool.query('SELECT * FROM farms', (err, results) => {
-		if (err) throw err;
-
-		res.status(200).json(results.rows);
+		if (err) {
+			console.log(err);
+			res.status(400).send(err).end();
+		} else {
+			res.status(200).json(results.rows);
+		}
 	});
 
 
