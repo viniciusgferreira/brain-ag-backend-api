a9ffc68816c9effa6da4d8e0d62448de8f456c37
ViniciusGFerreira <vinicius.gf@outlook.com>
2022-11-24 07:16:15 -0300

    route for put requests

--- src/app/routes/router.js
+++ src/app/routes/router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import addFarm from '../services/farms/addFarm.js';
 import listFarmById from '../services/farms/listFarmById.js';
 import listFarms from '../services/farms/listFarms.js';
+import editFarm from '../services/farms/editFarm.js';
 
 export const router = Router();
 
@@ -12,5 +13,5 @@ router.get('/farms/:id', listFarmById);
 //ADD NEW FARM
 router.post('/farms', addFarm);
 //EDIT FARM INFO
-
+router.put('farms', editFarm);
 //DELETE FARM
